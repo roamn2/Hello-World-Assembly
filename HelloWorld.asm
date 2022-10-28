680b6da2698276f09779fe359361324159a0e4e2
roamn2 <99689942+roamn2@users.noreply.github.com>
2022-10-28 18:51:29 +0200

    Add files via upload

---
+++ HelloWorld.asm
@@ -0,0 +1,18 @@
+section .bss
+section .data
+	hello: db "Hello, World!",14
+	helloLen: equ $-hello
+section .text
+	global _start
+
+	_start:
+		mov rax,1
+		mov rdi,1
+		mov rsi,hello
+		mov rdx,helloLen
+		syscall
+
+		mov rax,60
+		mov rdi,0
+		syscall
+
